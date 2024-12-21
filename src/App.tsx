856553be9249744c8207c07794d65145d935c2bd
Ysf_eazytec <b1176410144@126.com>
2024-12-21 12:20:49 +0800

    feat: 集成 React Query 进行服务端状态管理
    - 在 App.tsx 中设置 QueryClient
    - 添加全局 React Query Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import { FC, memo, ReactNode } from "react";
 import { Outlet } from "react-router-dom";
+import AppHeader from "@/components/app-header";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AppPlayBar from "@/components/player/app-play-bar";
+import AppFooter from "@/components/app-footer";
 
 interface IProps {
   children?: ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 const App: FC<IProps> = () => {
+
   return (
-    <div>
-      This is App
+    <QueryClientProvider client={queryClient}>
+      <AppHeader />
       <Outlet />
-    </div>
+      <AppFooter />
+      <AppPlayBar />
+    </QueryClientProvider>
   );
 };
 
